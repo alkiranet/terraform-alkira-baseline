6c67335564cca5bb7619d0e269f0e497a4098dd7
wcollins <william.levi.collins@gmail.com>
2022-07-14 12:14:35 -0400

    Add example outputs.tf

---
+++ examples/outputs.tf
@@ -0,0 +1,15 @@
+output "billing_tag_id" {
+  value = module.baseline.billing_tag_id
+}
+
+output "connector_group_id" {
+  value = module.baseline.connector_group_id
+}
+
+output "user_group_id" {
+  value = module.baseline.user_group_id
+}
+
+output "segment_id" {
+  value = module.baseline.segment_id
+}

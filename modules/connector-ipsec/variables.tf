b7998f99ea310265cdbd498a44669a4f921c34bd
wcollins <william.levi.collins@gmail.com>
2024-02-23 16:22:39 -0500

    Add submodule variables.tf

---
+++ modules/connector-ipsec/variables.tf
@@ -0,0 +1,38 @@
+variable "connector_ipsec" {
+  description = "Controls if IPSec connectors are created"
+  type        = bool
+  default     = false
+}
+
+variable "connector_ipsec_data" {
+  type = list(object({
+
+    # base vars
+    cxp         = string
+    customer_gateway_asn = string
+    enabled     = optional(string, true)
+    group       = optional(string)
+    name        = string
+    segment     = string
+    size        = optional(string, "SMALL")
+
+    # endpoint vars
+    endpoints  = optional(list(object({
+      billing_tags              = optional(list(string), [])
+      customer_gateway_ip       = string
+      enable_tunnel_redundancy  = optional(string, true)
+      name                      = string
+      preshared_keys            = optional(list(string), [])
+    })))
+
+    # routing option vars
+    routing_options  = optional(object({
+      availability          = optional(string, "IPSEC_INTERFACE_PING")
+      bgp_auth_key          = optional(string)
+      customer_gateway_asn  = string
+      type                  = optional(string, "DYNAMIC")
+    }))
+
+  }))
+  default = []
+}

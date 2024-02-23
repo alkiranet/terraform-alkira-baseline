0f832afdc9504fd2c2a2ebbdc14de21f1cdc6ecc
wcollins <william.levi.collins@gmail.com>
2024-02-23 16:22:16 -0500

    Add submodule main.tf

---
+++ modules/connector-ipsec/main.tf
@@ -0,0 +1,106 @@
+locals {
+
+  # filter 'segment' data
+  filter_segments   = var.connector_ipsec_data[*].segment
+
+  # filter 'billing_tag' data
+  filter_billing_tags = flatten([
+    for connector in var.connector_ipsec_data : [
+      for endpoint in connector.endpoints : [
+        for billing_tag in endpoint.billing_tags : billing_tag
+      ]
+    ]
+  ])
+
+}
+
+data "alkira_segment" "segment" {
+
+  for_each = toset(local.filter_segments)
+
+  name = each.value
+
+}
+
+data "alkira_billing_tag" "billing_tag" {
+
+  for_each = {
+    for k, v in toset(local.filter_billing_tags) : k => v
+  }
+
+  name = each.key
+
+}
+
+/*
+alkira_connector_ipsec
+https://registry.terraform.io/providers/alkiranet/alkira/latest/docs/resources/connector_ipsec
+*/
+locals {
+
+  filter_ipsec_connectors = flatten([
+    for c in var.connector_ipsec_data : {
+
+        # filter base configuration
+        cxp            = upper(c.cxp)
+        enabled        = c.enabled
+        group          = c.group
+        name           = c.name
+        segment        = lookup(data.alkira_segment.segment, c.segment, null).id
+        size           = c.size
+        
+        # filter endpoints
+        endpoints  = [for endpoint in c.endpoints : {
+          billing_tags              = [for billing_tag in endpoint.billing_tags : lookup(data.alkira_billing_tag.billing_tag, billing_tag, null).id]
+          customer_gateway_ip       = endpoint.customer_gateway_ip
+          enable_tunnel_redundancy  = endpoint.enable_tunnel_redundancy
+          name                      = endpoint.name
+          preshared_keys            = endpoint.preshared_keys
+      }]
+
+      # filter routing options
+      routing_options = {
+        availability          = c.routing_options["availability"]
+        bgp_auth_key          = c.routing_options["bgp_auth_key"]
+        customer_gateway_asn  = c.routing_options["customer_gateway_asn"]
+        type                  = c.routing_options["type"]
+      }
+
+    }
+  ])
+}
+
+resource "alkira_connector_ipsec" "connector" {
+
+  for_each = {
+    for o in local.filter_ipsec_connectors : o.name => o
+    if var.connector_ipsec == true
+  }
+
+  cxp         = upper(each.value.cxp)
+  enabled     = each.value.enabled
+  group       = each.value.group
+  name        = each.value.name
+  segment_id  = each.value.segment
+  size        = each.value.size
+  vpn_mode    = "ROUTE_BASED"
+
+  dynamic "endpoint" {
+    for_each = each.value.endpoints
+    content {
+      billing_tag_ids           = endpoint.value.billing_tags
+      customer_gateway_ip       = endpoint.value.customer_gateway_ip
+      enable_tunnel_redundancy  = endpoint.value.enable_tunnel_redundancy
+      name                      = endpoint.value.name
+      preshared_keys            = endpoint.value.preshared_keys
+    }
+  }
+
+  routing_options {
+    availability          = each.value.routing_options.availability
+    bgp_auth_key          = each.value.routing_options.bgp_auth_key
+    customer_gateway_asn  = each.value.routing_options.customer_gateway_asn
+    type                  = each.value.routing_options.type
+  }
+
+}
